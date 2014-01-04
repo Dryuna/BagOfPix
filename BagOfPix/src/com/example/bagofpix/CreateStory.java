60a2492e395ba2ec76c8c050b08227b12b36598a
AmrARaouf <amr.abdelraouf93@gmail.com>
2014-01-05 00:46:13 +0200

    adds create story w/o db connection

--- BagOfPix/src/com/example/bagofpix/CreateStory.java
+++ BagOfPix/src/com/example/bagofpix/CreateStory.java
@@ -2,28 +2,26 @@ package com.example.bagofpix;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 
-public class MainActivity extends Activity {
+public class CreateStory extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_create_story);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		getMenuInflater().inflate(R.menu.create_story, menu);
 		return true;
 	}
 	
-	public void newStory(View view) {
-		Intent intent = new Intent(this, CreateStory.class);
-	    startActivity(intent);
+	public void createStory(View view) {
+		// TODO
 	}
 
 }
